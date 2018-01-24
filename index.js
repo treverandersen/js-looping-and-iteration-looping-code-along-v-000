32940f851526630545d83eaac6e1b82dc0b5289c
Trever Andersen <trever.r.andersen@gmail.com>
2018-01-24 22:30:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${[i+1]}.`);
+  }
+  return names;
+}
+
+function tailsNeverFails() {
+  let t = 0;
+  while (Math.random() >= 0.5) {
+    t++
+
+  }
+  return `You got ${t} tails in a row!`;
+}
